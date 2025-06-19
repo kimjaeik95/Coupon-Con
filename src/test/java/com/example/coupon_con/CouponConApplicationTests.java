ec1d60ee9321bdad129de7402840ee45e9ab0cda
kimjaeik95 <wodlrdlsla@naver.com>
2025-06-19 21:09:59 +0900

    feat : 플라이웨이 초기 마이그레이션 추가 - 쿠폰,쿠폰발급 DB 테이블 생성

---
+++ src/test/java/com/example/coupon_con/CouponConApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coupon_con;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponConApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
