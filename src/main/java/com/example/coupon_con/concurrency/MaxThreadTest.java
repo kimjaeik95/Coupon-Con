64b40563643c01157d17dd6ba0491def5fc108d8
kimjaeik95 <wodlrdlsla@naver.com>
2025-06-19 22:06:14 +0900

    feat: JVM/OS 환경 최대 스레드 수 확인용 테스트 코드 추가

---
+++ src/main/java/com/example/coupon_con/concurrency/MaxThreadTest.java
@@ -0,0 +1,12 @@
+package com.example.coupon_con.concurrency;/**
+ * packageName    : com.example.coupon_con.concurrency
+ * fileName       : MaxThreadTest
+ * author         : JAEIK
+ * date           : 6/19/25
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 6/19/25       JAEIK       최초 생성
+ */public class MaxThreadTest {
+}
