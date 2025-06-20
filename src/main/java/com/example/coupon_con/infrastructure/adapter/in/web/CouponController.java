15d46d34b79ca01485bc8ed66981dc70b0b8c463
kimjaeik95 <wodlrdlsla@naver.com>
2025-06-20 21:09:37 +0900

    refactor: 모듈 구조 변경 - MVC 패턴에서 핵사고날 아키텍처로 전환

---
+++ src/main/java/com/example/coupon_con/infrastructure/adapter/in/web/CouponController.java
@@ -0,0 +1,12 @@
+package com.example.coupon_con.infrastructure.adapter.in.web;/**
+ * packageName    : com.example.coupon_con.infrastructure.adapter.in.web
+ * fileName       : CouponController
+ * author         : JAEIK
+ * date           : 6/20/25
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 6/20/25       JAEIK       최초 생성
+ */public class CouponController {
+}
