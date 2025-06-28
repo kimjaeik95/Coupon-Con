665bc656f4d5e71abcf602c9e97240c3cacfc22c
kimjaeik95 <wodlrdlsla@naver.com>
2025-06-28 18:15:02 +0900

    feat: 멤버 쿠폰 발급 Api 기능 구현

---
+++ src/main/java/com/example/coupon_con/application/port/in/IssueCouponToMemberUseCase.java
@@ -0,0 +1,12 @@
+package com.example.coupon_con.application.port.in;/**
+ * packageName    : com.example.coupon_con.application.port.in
+ * fileName       : IssueCouponToMemberUseCase
+ * author         : JAEIK
+ * date           : 6/28/25
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 6/28/25       JAEIK       최초 생성
+ */public interface IssueCouponToMemberUseCase {
+}
---
+++ src/main/java/com/example/coupon_con/infrastructure/adapter/out/persistence/mapper/MemberCouponIssueMapper.java
@@ -0,0 +1,12 @@
+package com.example.coupon_con.infrastructure.adapter.out.persistence.mapper;/**
+ * packageName    : com.example.coupon_con.infrastructure.adapter.out.persistence.mapper
+ * fileName       : MemberCouponIssueMapper
+ * author         : JAEIK
+ * date           : 6/28/25
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 6/28/25       JAEIK       최초 생성
+ */public interface MemberCouponIssueMapper {
+}
