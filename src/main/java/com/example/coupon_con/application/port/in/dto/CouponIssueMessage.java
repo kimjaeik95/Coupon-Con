89a38861450bf42fb6777921cb3262117454b8ea
kimjaeik95 <wodlrdlsla@naver.com>
2026-07-02 01:53:51 +0900

    refactor : RabbitMQ 상수 기반 설정을 Properties 기반 설정으로 변경 - config 클래스에서 상수 직접 참조 제거 - application.yml 기반 설정 바인딩 구조 도입 - config 설정과 상수 책임 분리 (단일책임원칙) - 추후 Queue/Exchange 확장 가능 구조로 개선

---
+++ src/main/java/com/example/coupon_con/application/port/in/dto/CouponIssueMessage.java
@@ -0,0 +1,12 @@
+package com.example.coupon_con.application.port.in.dto;/**
+ *packageName    : com.example.coupon_con.application.port.in.dto
+ * fileName       : CouponIssueMessage
+ * author         : JAEIK
+ * date           : 7/2/26
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 7/2/26        JAEIK       최초 생성
+ */public class CouponIssueMessage {
+}
---
+++ src/main/java/com/example/coupon_con/infrastructure/properties/CouponRabbitProperties.java
@@ -0,0 +1,24 @@
+package com.example.coupon_con.infrastructure.properties;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * packageName    : com.example.coupon_con.infrastructure.properties
+ * fileName       : CouponRabbitProperties
+ * author         : JAEIK
+ * date           : 7/2/26
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 7/2/26        JAEIK       최초 생성
+ */
+@Getter
+@ConfigurationProperties(prefix = "rabbitmq.coupon")
+public class CouponRabbitProperties {
+    private String exchange;
+    private String queue;
+    private String routingKey;
+
+}
