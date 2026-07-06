46479f0ae7763d3ffb641d522b8b4b9fb2272b72
kimjaeik95 <wodlrdlsla@naver.com>
2026-07-07 01:39:45 +0900

    feat: 회원별 쿠폰 발급 상태 관리를 위한 SseEmitterRepository 구현

---
+++ src/main/java/com/example/coupon_con/infrastructure/adapter/in/web/CouponPageController.java
@@ -0,0 +1,12 @@
+package com.example.coupon_con.infrastructure.adapter.in.web;/**
+ *packageName    : com.example.coupon_con.infrastructure.adapter.in.web
+ * fileName       : CouponPageController
+ * author         : JAEIK
+ * date           : 7/6/26
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 7/6/26        JAEIK       최초 생성
+ */public class CouponPageController {
+}
